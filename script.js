c16065097f11a26312a45cbd48517783426bdb4f
Bibhanshu Mishra <bibhu0404@gmail.com>
2025-03-15 15:34:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const navLinks = document.querySelectorAll('nav ul li a');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', function () {
+    // Remove active class from all links
+    navLinks.forEach(nav => nav.classList.remove('active'));
+    // Add active class to the clicked link
+    this.classList.add('active');
+  });
+});
